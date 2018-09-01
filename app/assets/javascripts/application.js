391fcd224d91e3d705b57c3e1d0917af004f82cb
kennylai5198 <kennylai.kimlung@outlook.com>
2018-09-01 12:42:46 +0800

    Added map feature and near-by rooms

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,5 +15,4 @@
 //= require jquery_ujs
 //= require toastr
 //= require activestorage
-//= require turbolinks
 //= require_tree .
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_25_042904) do
+ActiveRecord::Schema.define(version: 2018_09_01_015704) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,8 @@ ActiveRecord::Schema.define(version: 2018_08_25_042904) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_rooms_on_user_id"
   end
 
