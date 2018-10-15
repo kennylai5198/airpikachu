c98faffc146e804685545af6fe1e385a9ce579e5
kennylai5198 <kennylai.kimlung@outlook.com>
2018-10-15 20:18:28 +0800

    Created Dashboard View

--- config/routes.rb
+++ config/routes.rb
@@ -30,4 +30,7 @@ Rails.application.routes.draw do
   get '/your_reservations' => 'reservations#your_reservations'
 
   get 'search' => 'pages#search'
+
+  get 'dashboard' => 'dashboards#index'
+
 end
