13b88c03aab4a37477b468e1c2d5fad0e05e8de0
kennylai5198 <kennylai.kimlung@outlook.com>
2018-10-27 19:48:26 +0800

    Improving Payment Form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,33 +42,27 @@ class UsersController < ApplicationController
   end
 
   def add_card
+
     if current_user.stripe_id.blank?
       customer = Stripe::Customer.create(
         email: current_user.email
       )
       current_user.stripe_id = customer.id
       current_user.save
+
+      # Add Credit Card to Stripe
+      customer.sources.create(source: params[:stripeToken])
     else
       customer = Stripe::Customer.retrieve(current_user.stripe_id)
+      customer.source = params[:stripeToken]
+      customer.save
     end
-
-    # Add Credit Card to Stripe
-    month, year = params[:expiry].split(/ \/ /)
-    new_token = Stripe::Token.create(:card => {
-      :number => params[:number],
-      :exp_month => month,
-      :exp_year => year,
-      :cvc => params[:cvv]
-    })
-
-    customer.sources.create(source: new_token.id)
 
     flash[:notice] = "Your card is saved."
     redirect_to payment_method_path
   rescue Stripe::CardError => e
     flash[:alert] = e.message
     redirect_to payment_method_path
-
   end
 
   private
